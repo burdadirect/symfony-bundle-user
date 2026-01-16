373fd244bbedeefaa669452aa28d8526591bcd4f
Joachim Krempel <joachim.krempel@burda.com>
2026-01-16 12:15:19 +0100

    Update password previously used constraint

--- src/Validator/Constraints/PasswordPreviouslyUsed.php
+++ src/Validator/Constraints/PasswordPreviouslyUsed.php
@@ -2,11 +2,23 @@
 
 namespace HBM\UserBundle\Validator\Constraints;
 
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 use Symfony\Component\Validator\Constraint;
 
 class PasswordPreviouslyUsed extends Constraint
 {
     public static string $message = 'Das Passwort wurde zuvor bereits genutzt. Das Passwort sollte sich von den letzten {{ num }} Passwörtern unterscheiden.';
+
+    #[HasNamedArguments]
+    public function __construct(?array $options = null, ?array $groups = null, mixed $payload = null)
+    {
+        if (\is_array($options)) {
+            trigger_deprecation('symfony/validator', '7.3', 'Passing an array of options to configure the "%s" constraint is deprecated, use named arguments instead.', static::class);
+        }
+
+        parent::__construct($options, $groups, $payload);
+    }
+
 
     public function getTargets(): string|array
     {
