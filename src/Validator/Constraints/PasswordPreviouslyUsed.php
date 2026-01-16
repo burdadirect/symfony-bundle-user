0a2d50a86c8b29dc8befdbac8065034588f28334
Joachim Krempel <joachim.krempel@burda.com>
2026-01-16 12:17:59 +0100

    Update password previously used constraint

--- src/Validator/Constraints/PasswordPreviouslyUsed.php
+++ src/Validator/Constraints/PasswordPreviouslyUsed.php
@@ -16,7 +16,7 @@ class PasswordPreviouslyUsed extends Constraint
             trigger_deprecation('symfony/validator', '7.3', 'Passing an array of options to configure the "%s" constraint is deprecated, use named arguments instead.', static::class);
         }
 
-        parent::__construct($options, $groups, $payload);
+        parent::__construct(null, $groups, $payload);
     }
 
 
